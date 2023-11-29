19162c12b5c2de925f6b13c6fa74a881a3679a25
jrdnspam <jspampin@gmail.com>
2023-11-29 14:15:21 -0500

    finished movie finder

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ class MovieFinder extends React.Component {
     this.state = {
       searchTerm: '',
       results: [],
+      error: '',
     };
 
     this.handleChange = this.handleChange.bind(this);
@@ -16,11 +17,38 @@ class MovieFinder extends React.Component {
 
   handleSubmit(event) {
     event.preventDefault();
-    //nothing yet prevents default
+    let { searchTerm } = this.state;
+    searchTerm = searchTerm.trim();
+    if (!searchTerm) {
+      return;
+    }
+
+    const checkStatus = (response) => {
+      if (response.ok) {
+        // .ok returns true if response status is 200-299
+        return response;
+      }
+      throw new Error('Request was either a 404 or 500');
+    }
+    const json = (response) => response.json()
+
+    fetch(`https://www.omdbapi.com/?s=${searchTerm}&apikey=46c42541`).then(checkStatus)
+    .then(json)
+    .then((data) => {
+      if (data.Response === 'False') {
+        throw new Error(data.Error);
+      }
+      if (data.Response === 'True' && data.Search) {
+        this.setState({ results: data.Search, error: '' })
+      }
+    }).catch((error) => {
+      this.setState({ error: error.message });
+      console.log(error);
+    })
   }
 
   render() {
-    const { searchTerm, results } = this.state;
+    const { searchTerm, results, error } = this.state;
 
     return (
       <div className="container">
@@ -36,15 +64,42 @@ class MovieFinder extends React.Component {
               />
               <button type="submit" className="btn btn-primary">Submit</button>
             </form>
-            {results.map((movie) => {
-              return null;  // returns nothing for now
-            })}
+            {(() => {
+              if (error) {
+                return error;
+              }
+              return results.map((movie) => {
+                return <Movie key={movie.imdbID} movie={movie} />;
+              })
+            })()}
           </div>
         </div>
       </div>
     )
   }
 }
+
+const Movie = (props) => {
+  const { Title, Year, imdbID, Type, Poster } = props.movie;
+
+  return (
+    <div className="row">
+      <div className="col-4 col-md-3 mb-3">
+        <a href={`https://www.imdb.com/title/${imdbID}/`} target="_blank">
+          <img src={Poster} className="img-fluid" />
+        </a>
+      </div>
+      <div className="col-8 col-md-9 mb-3">
+        <a href={`https://www.imdb.com/title/${imdbID}/`} target="_blank">
+          <h4>{Title}</h4>
+          <p>{Type} | {Year}</p>
+        </a>
+      </div>
+    </div>
+
+  )
+}
+
 
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
