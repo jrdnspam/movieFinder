83f441c4f936d161fa87226afd71f95c6d33bb72
jrdnspam <jspampin@gmail.com>
2023-11-30 12:05:14 -0500

    todolist

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
-class MovieFinder extends React.Component {
+const checkStatus = (response) => {
+  if (response.ok) {
+    return response;
+  }
+  throw new Error('Request was either a 404 or 500');
+}
+
+const json = (response) => response.json()
+
+class ToDoList extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      searchTerm: '',
-      results: [],
-      error: '',
+      new_task: '',
+      tasks: [],
     };
 
     this.handleChange = this.handleChange.bind(this);
@@ -12,66 +20,34 @@ class MovieFinder extends React.Component {
   }
 
   handleChange(event) {
-    this.setState({ searchTerm: event.target.value });
+    this.setState({ new_task: event.target.value });
   }
 
   handleSubmit(event) {
     event.preventDefault();
-    let { searchTerm } = this.state;
-    searchTerm = searchTerm.trim();
-    if (!searchTerm) {
-      return;
-    }
-
-    const checkStatus = (response) => {
-      if (response.ok) {
-        // .ok returns true if response status is 200-299
-        return response;
-      }
-      throw new Error('Request was either a 404 or 500');
-    }
-    const json = (response) => response.json()
-
-    fetch(`https://www.omdbapi.com/?s=${searchTerm}&apikey=46c42541`).then(checkStatus)
-    .then(json)
-    .then((data) => {
-      if (data.Response === 'False') {
-        throw new Error(data.Error);
-      }
-      if (data.Response === 'True' && data.Search) {
-        this.setState({ results: data.Search, error: '' })
-      }
-    }).catch((error) => {
-      this.setState({ error: error.message });
-      console.log(error);
-    })
   }
 
   render() {
-    const { searchTerm, results, error } = this.state;
+    const { new_task, tasks } = this.state;
 
     return (
       <div className="container">
         <div className="row">
           <div className="col-12">
+            <h2 className="mb-3">To Do List</h2>
+            {tasks.length > 0 ? tasks.map((task) => {
+              return null; // return nothing for now
+            }) : <p>no tasks here</p>}
             <form onSubmit={this.handleSubmit} className="form-inline my-4">
               <input
                 type="text"
-                className="form-control mr-sm-2"
-                placeholder="frozen"
-                value={searchTerm}
+                className="form-control mr-sm-2 mb-2"
+                placeholder="new task"
+                value={new_task}
                 onChange={this.handleChange}
               />
-              <button type="submit" className="btn btn-primary">Submit</button>
+              <button type="submit" className="btn btn-primary mb-2">Submit</button>
             </form>
-            {(() => {
-              if (error) {
-                return error;
-              }
-              return results.map((movie) => {
-                return <Movie key={movie.imdbID} movie={movie} />;
-              })
-            })()}
           </div>
         </div>
       </div>
@@ -79,28 +55,6 @@ class MovieFinder extends React.Component {
   }
 }
 
-const Movie = (props) => {
-  const { Title, Year, imdbID, Type, Poster } = props.movie;
-
-  return (
-    <div className="row">
-      <div className="col-4 col-md-3 mb-3">
-        <a href={`https://www.imdb.com/title/${imdbID}/`} target="_blank">
-          <img src={Poster} className="img-fluid" />
-        </a>
-      </div>
-      <div className="col-8 col-md-9 mb-3">
-        <a href={`https://www.imdb.com/title/${imdbID}/`} target="_blank">
-          <h4>{Title}</h4>
-          <p>{Type} | {Year}</p>
-        </a>
-      </div>
-    </div>
-
-  )
-}
-
-
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
-root.render(<MovieFinder />);
+root.render(<ToDoList />);
